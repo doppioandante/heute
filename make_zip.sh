0574d4f27ee8348271ebfa6759a76a3115433930
Enrico Lumetti <enrico.lumetti@gmail.com>
2017-09-11 17:02:26 +0200

    Bump version

--- make_zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Remember to tag a new version and to update the manifest!"
-zip heute.zip countdown_overlay.css countdown_overlay.js heute.js landing.html manifest.json options.html options.js
-
